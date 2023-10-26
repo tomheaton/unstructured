4f04eeb8ae6fd91ab5430a9a0d8f763d78be020a
cragwolfe <crag@unstructured.io>
2023-10-26 11:10:51 -0700

    crag/ideation-different-eval-types

--- test_unstructured_ingest/evaluation-metrics.sh
+++ test_unstructured_ingest/evaluation-metrics.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# first argument is the type of evaluation
+EVAL_TYPE=$ARGV[0]
+
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 cd "$SCRIPT_DIR"/.. || exit 1
 
@@ -11,10 +14,10 @@ mkdir -p "$OUTPUT_DIR"
 
 # Download cct test from s3
 BUCKET_NAME=utic-dev-tech-fixtures
-FOLDER_NAME=small-cct
-CCT_DIR=$SCRIPT_DIR/gold-standard/$FOLDER_NAME
-mkdir -p "$CCT_DIR"
-aws s3 cp "s3://$BUCKET_NAME/$FOLDER_NAME" "$CCT_DIR" --recursive --no-sign-request --region us-east-2
+FOLDER_NAME=small-"$EVAL_TYPE"
+LOCAL_GOLD_STANDARD_DIR=$SCRIPT_DIR/gold-standard/"$EVAL_TYPE"/$FOLDER_NAME
+mkdir -p "$LOCAL_DIR"
+aws s3 cp "s3://$BUCKET_NAME/$FOLDER_NAME" "$LOCAL_GOLD_STANDARD_DIR" --recursive --no-sign-request --region us-east-2
 
 # shellcheck disable=SC1091
 source "$SCRIPT_DIR"/cleanup.sh
@@ -25,7 +28,7 @@ function cleanup() {
 trap cleanup EXIT
 
 EXPORT_DIR="$SCRIPT_DIR"/metrics
-PYTHONPATH=. ./unstructured/ingest/evaluate.py \
+PYTHONPATH=. ./unstructured/ingest/evaluate.py "$EVAL_TYPE" \
     --output_dir "$OUTPUT_DIR" \
     --source_dir "$CCT_DIR" \
     --export_dir "$EXPORT_DIR"
